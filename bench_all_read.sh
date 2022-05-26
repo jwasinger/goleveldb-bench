0c7a962ecb473c007f99b2d8ae9c44a33e52342b
Jared Wasinger <j-wasinger@hotmail.com>
2022-05-26 06:16:15 +0000

    update charts and datasets

--- bench_all_read.sh
+++ bench_all_read.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-# ./run_read_bench.sh random-read
+./run_read_bench.sh random-read
 ./run_read_bench.sh random-read-filter
 ./run_read_bench.sh random-read-bigcache
 ./run_read_bench.sh random-read-bigcache-filter
--- run_read_bench.sh
+++ run_read_bench.sh
@@ -3,13 +3,17 @@
 # run pebble
 rm -rf testdb-*
 mkdir -p datasets/mymachine-1gb
-mkdir -p readcharts
+mkdir -p read-charts
 
 ./cmd/pebble-readbench/pebble-readbench -size 1gb -logdir datasets/mymachine-1gb -test $1
 ./cmd/ldb-benchplot/ldb-benchplot -out ./read-charts/$1-pebble.svg datasets/mymachine-1gb/$1.json
+
+mv datasets/mymachine-1gb/$1.json datasets/mymachine-1gb/$1-pebble.json
 
 # run ldb
 rm -rf testdb-*
 
 ./cmd/ldb-readbench/ldb-readbench -size 1gb -logdir datasets/mymachine-1gb -test $1
 ./cmd/ldb-benchplot/ldb-benchplot -out ./read-charts/$1-ldb.svg datasets/mymachine-1gb/$1.json
+
+mv datasets/mymachine-1gb/$1.json datasets/mymachine-1gb/$1-ldb.json
